a64aa43c4c5711761e847562c5f56e0192c60a91
hamzahassan4925-stack <hamzahassan4925@gmail.com>
2026-09-25 20:37:46 +0500

    Add files via upload

---
+++ lecturer5.py
@@ -0,0 +1,76 @@
+print("Hello, World!")
+name ="hassan"
+name1 = 'hassanbabar'
+name2 = "'good boy'"
+age = 19
+price = -7.99
+print(name)
+print(name1)
+print(name2)
+print(age)
+print(price)
+print(type(price))
+print(type(name2))
+old = True
+print(type(old))
+a = None
+print(type(a))
+a = 3
+b = 7
+sum = a-b
+print(sum)
+print(a+b)
+#kia dhake mari python
+"""mera name hasssan ha pythonseekh raha ho bhot
+maza aa rhaha hai python seekh kay"""
+print("hello world")
+#arthamatic operations
+a = 20
+b = 4
+sum =a+b
+print(sum)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+#relational operators
+a = 20
+b=10
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+#assingment operators
+num = 100
+num =100+100
+print(num)
+num+=100
+print(num)
+num-=50
+print(num)
+num*=2
+print(num)
+num/=4
+print(num)
+num%=5
+print(num)
+num**=5
+print(num)
+#logical operators
+print(not True)
+print(not False)
+a=50
+b=30
+print(not(a>b))
+val1=11
+val2=5
+print(val1 and val2)
+print("or operator",val1 or val2)
+print((val1==val2) or (val1>val2))
+
+
+
+
+
