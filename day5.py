fa175895eb0fc4ae07cbdd5d59675f1ee760bc3c
hamzahassan4925-stack <hamzahassan4925@gmail.com>
2026-09-26 20:33:25 +0500

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,28 @@
+#type conversion
+from os import name
+
+
+a = 4
+b = float("8")
+sum = (a+b)
+print(sum)
+print(type(sum))
+val1 = 23
+val1 = str(val1)
+print(val1)
+print(type(val1))
+# learning about input function  
+# learning about input function
+name = input("enter your name: ")
+print("you are gay " + name)
+
+age = float(input("enter your age: "))
+print("your age is " + str(age))  # float ko str me convert kiya jodne ke liye
+print(type(age))
+
+
+
+
+
+
+
